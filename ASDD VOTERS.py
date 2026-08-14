5942e5afe3c14f3f06168fdc4779f8ade48e31b3
abhishekmann91 <49642259+abhishekmann91@users.noreply.github.com>
2026-08-14 17:37:34 +0530

    Update ASDD VOTERS.py

--- ASDD VOTERS.py
+++ ASDD VOTERS.py
@@ -28,7 +28,7 @@ METADATA_FILE = "master_voter_pdf_metadata.txt"
 CATEGORIES = {
     "Permanently Shifted": {
         "annexure": "Annexure-II",
-        "title": "List of ASDD Electors(As generated from ECINet) Handed over to BLAs: Category - Permanently Shifted",
+        "title": "List of ASDD Electors(As generated from ECINet) Handed over to BLAs:\nCategory - Permanently Shifted",
         "last_col": "Category / Remarks",
         "default_remark": "Permanently Shifted",
         "filename_suffix": "Permanently_Shifted"
